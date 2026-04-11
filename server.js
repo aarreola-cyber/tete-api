aa62792b4011c1572880cc7eac3d146eedad21d3
aarreola-cyber <aarreola@gmail.com>
2026-04-11 15:22:17 -0600

    Update server.js

--- server.js
+++ server.js
@@ -10,9 +10,13 @@ app.get("/", (req, res) => {
   res.send("API funcionando");
 });
 
+app.get("/mensaje", (req, res) => {
+  res.json({ text: "ya volviste… sabía que no ibas a aguantar" });
+});
+
 app.post("/mensaje", (req, res) => {
   res.json({ text: "ya volviste… sabía que no ibas a aguantar" });
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("running on " + PORT));
+app.listen(PORT, () => console.log("running"));
