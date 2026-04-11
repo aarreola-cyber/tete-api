609be18d380bc948bfb43c61bbdc4510da46b698
aarreola-cyber <aarreola@gmail.com>
2026-04-11 15:13:20 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-import express from "express";
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 app.post("/mensaje", (req, res) => {
